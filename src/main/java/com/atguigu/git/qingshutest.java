c4d03c1342a90802741b9a67d8cc4a09b6c63602
dongchao <17602608027@163.com>
2020-03-29 22:30:59 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/qingshutest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Author: dongbiao
+ * Date:
+ * Desc:
+ */
+public class qingshutest {
+    public static void main(String[] args) {
+        System.out.println("眼里都是你，亿万星辰犹不及");
+    }
+}
